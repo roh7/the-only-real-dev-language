c0a6f01a990d9612fe450a472f256a468bd72a03
Rohan Kopparapu <rohankops.14@gmail.com>
2015-05-29 13:10:45 +0100

    first commit

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
-cloud-to-butt
+the-only-real-dev-language
 =============
 
-Chrome extension that replaces occurrences of 'the cloud' with 'my butt'
-
-[Direct download of crx file](https://github.com/panicsteve/cloud-to-butt/blob/master/CloudToButt.crx?raw=true)
-
-Note that there are forks of this extension that simply replace 'cloud' with 'butt'.
-In my personal opinion, that approach is too broad and it's less funny as a result, but it is clearly a very
-polarizing issue in the cloud-to-butt user community.  Forks are free to do whatever they like.  But officially, _this_
-extension replaces only the phrase described above. Thank you for your concern.
-
-Screenshot Gallery
-------------------
-
-http://www.flickr.com/groups/cloud-to-butt/
+Chrome extension that replaces occurrences of 'node.js' with 'the only real dev language'
 
 Installation
 ------------
@@ -23,15 +11,15 @@ In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that 
 Safari Version
 --------------
 
-Can be found here: https://github.com/logancollins/cloud-to-butt-safari
+Doesn't exist....yet!
 
 Firefox Version
 ---------------
 
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-mozilla
+Doesn't exist....yet!
 
 
 Opera Version
 ---------------
 
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-opera
+Doesn't exist....yet!
--- Source/content_script.js
+++ Source/content_script.js
@@ -2,41 +2,52 @@ walk(document.body);
 
 function walk(node) 
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+    // Source: http://is.gd/mwZp7E
+    
+    var child, next;
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+    switch ( node.nodeType )  
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
 }
 
 function handleText(textNode) 
 {
-	var v = textNode.nodeValue;
+    var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+    v = v.replace(/\Node.js\b/g, "The only real dev language");
+    v = v.replace(/\bnode.js\b/g, "the only real dev language");
+    v = v.replace(/\bNODE.js\b/g, "The only real dev language");
+    v = v.replace(/\bnode.JS\b/g, "the only real dev language");
+    v = v.replace(/\bNODE.JS\b/g, "THE ONLY REAL DEV LANGUAGE");
+
+    v = v.replace(/\bNode js\b/g, "The only real dev language");
+    v = v.replace(/\bnode js\b/g, "the only real dev language");
+    v = v.replace(/\bNODE js\b/g, "The only real dev language");
+    v = v.replace(/\bnode JS\b/g, "the only real dev language");
+    v = v.replace(/\bNODE JS\b/g, "THE ONLY REAL DEV LANGUAGE");    
+
+
+    v = v.replace(/\Nodejs\b/g, "The only real dev language");
+    v = v.replace(/\nodejs\b/g, "the only real dev language");
+    v = v.replace(/\NodeJS\b/g, "The only real dev language");
+    v = v.replace(/\nodeJS\b/g, "the only real dev language");
+    v = v.replace(/\NODEJS\b/g, "THE ONLY REAL DEV LANGUAGE");
+    
+    textNode.nodeValue = v;
 }
-
-
